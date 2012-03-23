f36ac5262b1f30a3fe755e3ccf57017589b86c93
arnablan <arnablan@70a868fe-475b-4a30-a34c-9a8cdf4ec761>
2012-03-23 14:18:01 +0000

    Prend le chemin relatif complet pour le passage en parametre des arguments a promethe. Permet de mettre le .prt sur ../file.prt par exemple.
    git-svn-id: svn+ssh://cvs-etis.ensea.fr/svn/SimulateurV1/themis/trunk@79 70a868fe-475b-4a30-a34c-9a8cdf4ec761

--- src/script.c
+++ src/script.c
@@ -31,9 +31,7 @@ void makefile_add_argument(FILE *makefile, char *file_path)
 
 	if (file_path[0] != 0)
 	{
-		basename = g_path_get_basename(file_path);
-		fprintf(makefile, " %s", basename);
-		g_free(basename);
+		fprintf(makefile, " %s", file_path);
 	}
 }
 
